b912fe9005d443bc284e7996a637719d05cd4cc0
joohyun7u <joohyun7u@gmail.com>
2024-03-16 00:32:08 +0900

    k400 audio apply

--- models/cast_Bsquare.py
+++ models/cast_Bsquare.py
@@ -1518,7 +1518,7 @@ def compo_cast_audio_Bsquare_CA0_base_patch16_224(pretrained=False, args=None, c
 # audio - single Spec
 
 @register_model
-def single_audio_Bsquare_CA9_base_patch16_224(pretrained=False, args=None, class_list=None, **kwargs):
+def cast_single_audio_Bsquare_CA9_base_patch16_224(pretrained=False, args=None, class_list=None, **kwargs):
     model = STCrossTransformer(
         patch_size=16, embed_dim=768, text_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), composition=False, audio_enabled=False, text_num_heads=12, CA=9, output_text_dim=768,
@@ -1531,6 +1531,22 @@ def cast_single_audio_Bsquare_CA9_down4_base_patch16_224(pretrained=False, args=
         patch_size=16, embed_dim=768, text_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), composition=False, audio_enabled=True, text_num_heads=12, CA=9, output_text_dim=768,
         prefix = 16, postfix = 16, spec_frames=1, attn_all_frame=True, down_ratio=4, **kwargs)
+    return model
+
+@register_model
+def cast_stacks_audio_Bsquare_CA9_base_patch16_224(pretrained=False, args=None, class_list=None, **kwargs):
+    model = STCrossTransformer(
+        patch_size=16, embed_dim=768, text_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), composition=False, audio_enabled=False, text_num_heads=12, CA=9, output_text_dim=768,
+        prefix = 16, postfix = 16, spec_frames=16, attn_all_frame=False, **kwargs)
+    return model
+
+@register_model
+def cast_stacks_audio_Bsquare_CA9_down4_base_patch16_224(pretrained=False, args=None, class_list=None, **kwargs):
+    model = STCrossTransformer(
+        patch_size=16, embed_dim=768, text_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), composition=False, audio_enabled=True, text_num_heads=12, CA=9, output_text_dim=768,
+        prefix = 16, postfix = 16, spec_frames=16, attn_all_frame=False, down_ratio=4, **kwargs)
     return model
 
 @register_model
